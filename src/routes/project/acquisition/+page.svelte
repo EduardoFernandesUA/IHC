3dfbf59c332f53c8e2eb428d42e07041a80f2c21
João Afonso <ferreiraafonsojoao@ua.pt>
2023-05-05 17:26:08 +0100

    bora

--- src/routes/project/acquisition/+page.svelte
+++ src/routes/project/acquisition/+page.svelte
@@ -1,14 +1,78 @@
 <script lang='ts'>
-    let width: number, height: string;
-    
-    $: height = (width*(3/4)) + 'px';
+  let width: number, height: string;
 
+  $: height = (width*(1/2)) + 'px';
     
+  let stream;
+  let recorder;
+  let chunks = [];
+  let isPlaying = false;
+  let isRecording = false;
+
+
+
+  async function openCamera() {
+    isPlaying = true;
+    try {
+      // Get access to the camera
+      stream = await navigator.mediaDevices.getUserMedia({ video: true });
+      const videoElement = document.querySelector('video');
+
+      // Display the camera stream in a video element
+      videoElement.srcObject = stream;
+
+      // Create a MediaRecorder instance to record the video
+      recorder = new MediaRecorder(stream);
+
+      // Listen for data available events and collect the video chunks
+      recorder.addEventListener('dataavailable', event => {
+        chunks.push(event.data);
+      });
+
+      // Start recording
+      recorder.start();
+    } catch (error) {
+      console.error('Error opening camera:', error);
+    }
+  }
+
+  async function startRecording() {
+  isRecording = true;
+  if (!isPlaying) {
+    // If camera is not open, open it
+    await openCamera();
+  }
+
+  // Show the pause button and hide the play button
+  const startRecord = document.getElementById('record');
+  startRecord.style.display = 'none';
+
+  if (recorder && recorder.state === 'inactive') {
+    try {
+      recorder.start();
+      console.log('Recording video ...');
+    } catch (error) {
+      console.error('Error starting recording:', error);
+    }
+  }
+}
+
+
+  async function stopRecording() {
+    if (recorder && recorder.state === 'recording') {
+      recorder.stop();
+      isRecording = false;
+      console.log('Stopped recording video.');
+    }
+  }
+
+
+  
 </script>
 
 
 <div class="row">
-	<div class="col-3">
+	<div class="col-3"  style="margin-right: 100px;">
 		<form>
 			<fieldset>
                 <h3 class="my-4">Options:</h3>
@@ -22,27 +86,34 @@
 				</div>
 				<div class="input-group">
 					<span class="input-group-text w-75">Count Down</span>
-					<input type="number" aria-label="First name" class="form-control" />
+					<input type="number" aria-label="First name" class="form-control" value="3"/>
 				</div>
 				<div class="input-group">
 					<span class="input-group-text w-75">Duration</span>
-					<input type="number" aria-label="First name" class="form-control" />
+					<input type="number" aria-label="First name" class="form-control" value="3"/>
 				</div>
 				<div class="input-group">
 					<span class="input-group-text w-75">Repetitions</span>
-					<input type="number" aria-label="First name" class="form-control" />
+					<input type="number" aria-label="First name" class="form-control" value="2"/>
 				</div>
-				<button type="submit" class="btn btn-dark w-100 text-center mt-2">Record</button>
-			</fieldset>
+				<button id="record" type="submit" class="btn btn-dark w-100 text-center mt-2" on:click={startRecording}>Record</button>
+        {#if isRecording}
+          <button id="StopRecord" type="submit" class="btn btn-dark w-100 text-center mt-2" on:click={stopRecording}>Stop Record</button>
+        {/if}
+	</fieldset>
 		</form>
 	</div>
-	<div class="col-9 bg-dark">
-    <div class="video-container">
-      <video id="video-preview" ></video>
-      <button id="record-btn">Record</button>
-      <button id="stop-btn">Stop</button>
-    </div>
+	<div class="col-8 " style="height:{height}" bind:offsetWidth={width}>
+    <div class="icon-container">
+      <video width="2640" height="480" style="height:{height}" bind:offsetWidth={width} autoplay muted></video>
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      {#if !isPlaying}
+        <img id="play" src="/src/routes/img/play.png" alt="Icon" class="icon" style="display: flex; justify-content: center; align-items: center;margin-right:880px;margin-top:170px;" on:click={openCamera}>
+      {:else}
+        <!-- <img id="pause" src="/src/routes/img/stop.png" alt="Icon" class="icon" style="display: flex; justify-content: center; align-items: center;" on:click={stopCamera}> -->
+      {/if} </div>
   </div>
+  
 
 </div>
 
@@ -50,36 +121,58 @@
 
 <div class="row mb-5">
     <h1>Records</h1>
-    <table class="table">
+    <table class="table" style="text-align:center">
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">First</th>
-            <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">Name</th>
+            <th scope="col">Class</th>
+            <th scope="col" >Duration (s)</th>
           </tr>
         </thead>
         <tbody>
           <tr>
             <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <td>video_#1</td>
+            <td>ThumbsUp</td>
+            <td>4</td>
           </tr>
           <tr>
             <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
+            <td>ThumbsDown123</td>
+            <td>ThumbsDown</td>
+            <td>3.5</td>
           </tr>
           <tr>
             <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
+            <td>video589</td>
+            <td>PeaceSign</td>
+            <td>8</td>
           </tr>
         </tbody>
       </table>
 </div>
 
 <style>
+  .icon-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  #play {
+    display: block;
+    margin: auto;
+    max-width: 10%;
+    max-height: 10%;
+  }
+
+  #pause {
+    display: block;
+    margin: auto;
+    max-width: 6.5%;
+    max-height: 6.5%;
+  }
+  
+
 </style>
--- src/routes/project/test/+page.svelte
+++ src/routes/project/test/+page.svelte
@@ -1,4 +1,81 @@
-Test
-<div style="height:100px">
+<script lang='ts'>
+    let width: number, height: string;
+  
+    $: height = (width*(1/2)) + 'px';
+      
+  </script>
+  
+  
+  <div class="row">
+      <div class="col-3"  style="margin-right: 100px;text-align:center">
+          <form>
+              <fieldset>
+                <div class="input-group mb-3" >
+                    <h5 >Media Pipe Feature Calculator</h5>
+                    <!--<label class="" for="inputGroupSelect01" style="background-color: white; color: black;">Media Pipe Feature Calculator</label>
+                    <select class="form-select h-100" id="inputGroupSelect01">
+                          <option selected>Choose...</option>
+                          <option value="1">Model 1</option>
+                          <option value="2">Model 2</option>
+                          <option value="3">Model 3</option>
+                        </select> -->
+                  </div>
+                  
+                <div class="input-group">
+                    <div class="input-group-text">
+                        <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
+                    </div>
+                    <span class="input-group-text" style="flex-grow: 1;">MediaPipe - Hand Gesture</span>
+                    
+                </div>
 
-</div>
+                <div class="input-group ">
+                    <div class="input-group-text">
+                        <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
+                    </div>
+                    <span class="input-group-text" style="flex-grow: 1;">MediaPipe - Pose Detection</span>
+                    
+                </div>
+
+                <div class="input-group">
+                    <div class="input-group-text">
+                        <input class="form-check-input mt-0" type="checkbox" value="" aria-label="Checkbox for following text input">
+                    </div>
+                    <span class="input-group-text" style="flex-grow: 1;">MediaPipe - Holistic Detection</span>
+                    
+                </div>
+              </fieldset>
+          </form>
+      </div>
+        <div class="col-8 bg-dark" style="height:{height}" bind:offsetWidth={width}>
+            <div class="icon-container">
+                <img src="/src/routes/img/play.png" alt="Icon" class="icon" style="filter: brightness(0) saturate(100%) invert(100%); display: flex; justify-content: center; align-items: center;top: 10%;transform: translateY(200%);">
+            </div>
+
+           <p  style="margin-top: 380px;text-align:center">
+                Guessed Class: Thumbsup <br>
+                Accuracy: 0.89
+            </p>
+          
+        </div>
+
+    
+  </div>
+  
+  
+  <style>
+    .icon-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  
+    .icon {
+      display: block;
+      margin: auto;
+      filter: invert(1);
+      max-width: 10%;
+      max-height: 10%;
+    }
+  </style>
+  
